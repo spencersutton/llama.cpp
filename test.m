c4352c2ce4ea403f3404331692a9024dd3d2a434
Spencer Sutton <Spencer.Sutton@precisely.com>
2023-06-27 17:54:04 -0400

    encoder

--- test.m
+++ test.m
@@ -7,20 +7,19 @@ int main(int argc, const char *argv[]) {
   id<MTLDevice> device = MTLCreateSystemDefaultDevice();
 
   NSError *error = nil;
-
   NSURL *url = [NSURL fileURLWithPath:@"test.metal"];
-  fprintf(stderr, "%s: loading '%s'\n", __func__, [url absoluteString].UTF8String);
 
   id<MTLLibrary> library = [device newLibraryWithURL:url error:&error];
   if (error) {
     fprintf(stderr, "%s: error: %s\n", __func__, [[error description] UTF8String]);
     exit(1);
   }
-  id<MTLCommandQueue> queue = [device newCommandQueue];
 
   id<MTLFunction> function = [library newFunctionWithName:@"kernel_function"];
   id<MTLComputePipelineState> pipeline = [device newComputePipelineStateWithFunction:function
                                                                                error:nil];
-
+  id<MTLCommandQueue> queue = [device newCommandQueue];
+  id<MTLCommandBuffer> command_buffer = [queue commandBuffer];
+  id<MTLComputeCommandEncoder> encoder = [command_buffer computeCommandEncoder];
   return 0;
 }
