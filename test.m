ccb5b8323c7f90b2ce61797df8b44e5814afdc80
Spencer Sutton <Spencer.Sutton@precisely.com>
2023-06-27 17:56:57 -0400

    err check

--- test.m
+++ test.m
@@ -16,10 +16,27 @@ int main(int argc, const char *argv[]) {
   }
 
   id<MTLFunction> function = [library newFunctionWithName:@"kernel_function"];
+  if (!function) {
+    fprintf(stderr, "%s: error: %s\n", __func__, "no kernel function");
+    exit(1);
+  }
+
   id<MTLComputePipelineState> pipeline = [device newComputePipelineStateWithFunction:function
-                                                                               error:nil];
+                                                                               error:&error];
+  if (error) {
+    fprintf(stderr, "%s: error: %s\n", __func__, [[error description] UTF8String]);
+    exit(1);
+  }
+
   id<MTLCommandQueue> queue = [device newCommandQueue];
   id<MTLCommandBuffer> command_buffer = [queue commandBuffer];
   id<MTLComputeCommandEncoder> encoder = [command_buffer computeCommandEncoder];
+
+  {};
+
+  [encoder endEncoding];
+  [command_buffer commit];
+  [command_buffer waitUntilCompleted];
+  printf("done\n");
   return 0;
 }
