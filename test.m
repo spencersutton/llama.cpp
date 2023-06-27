f179450228f6b77e673e2465e55adf4e37399567
Spencer Sutton <Spencer.Sutton@precisely.com>
2023-06-27 18:24:58 -0400

    test

--- test.m
+++ test.m
@@ -4,39 +4,44 @@
 #import <MetalPerformanceShaders/MetalPerformanceShaders.h>
 
 int main(int argc, const char *argv[]) {
-  id<MTLDevice> device = MTLCreateSystemDefaultDevice();
+  @autoreleasepool {
+    id<MTLDevice> device = MTLCreateSystemDefaultDevice();
 
-  NSError *error = nil;
-  NSURL *url = [NSURL fileURLWithPath:@"test.metal"];
+    NSError *error = nil;
+    NSURL *url = [NSURL fileURLWithPath:@"test.metal"];
 
-  id<MTLLibrary> library = [device newLibraryWithURL:url error:&error];
-  if (error) {
-    fprintf(stderr, "%s: error: %s\n", __func__, [[error description] UTF8String]);
-    exit(1);
-  }
+    printf("loading library from %s\n", [[url absoluteString] UTF8String]);
+    // NSBundle *bundle = [NSBundle mainBundle];
+    id<MTLLibrary> library = [device newLibraryWithFile:@"default.metallib" error:&error];
+    if (library == nil || error != nil) {
+      fprintf(stderr, "%s:%d: error: %s\n", __FILE__, __LINE__, [[error description] UTF8String]);
+      exit(1);
+    }
 
-  id<MTLFunction> function = [library newFunctionWithName:@"kernel_function"];
-  if (!function) {
-    fprintf(stderr, "%s: error: %s\n", __func__, "no kernel function");
-    exit(1);
-  }
+    id<MTLFunction> function = [library newFunctionWithName:@"add_arrays"];
+    if (function == nil) {
+      fprintf(stderr, "%s:%d: error: function not found\n", __FILE__, __LINE__);
+      exit(1);
+    }
 
-  id<MTLComputePipelineState> pipeline = [device newComputePipelineStateWithFunction:function
-                                                                               error:&error];
-  if (error) {
-    fprintf(stderr, "%s: error: %s\n", __func__, [[error description] UTF8String]);
-    exit(1);
-  }
+    id<MTLComputePipelineState> pipeline = [device newComputePipelineStateWithFunction:function
+                                                                                 error:&error];
+    if (pipeline == nil || error != nil) {
+      fprintf(stderr, "%s:%d: error: %s\n", __FILE__, __LINE__, [[error description] UTF8String]);
+      exit(1);
+    }
 
-  id<MTLCommandQueue> queue = [device newCommandQueue];
-  id<MTLCommandBuffer> command_buffer = [queue commandBuffer];
-  id<MTLComputeCommandEncoder> encoder = [command_buffer computeCommandEncoder];
+    id<MTLCommandQueue> queue = [device newCommandQueue];
 
-  {};
+    id<MTLCommandBuffer> command_buffer = [queue commandBuffer];
+    id<MTLComputeCommandEncoder> encoder = [command_buffer computeCommandEncoder];
 
-  [encoder endEncoding];
-  [command_buffer commit];
-  [command_buffer waitUntilCompleted];
-  printf("done\n");
+    {};
+
+    [encoder endEncoding];
+    [command_buffer commit];
+    [command_buffer waitUntilCompleted];
+    printf("done\n");
+  };
   return 0;
 }
