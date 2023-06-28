b1dae2e89575311c4f6212d49fd02575d4a76e97
Spencer Sutton <Spencer.Sutton@precisely.com>
2023-06-28 00:53:40 -0400

    test

--- test.m
+++ test.m
@@ -31,11 +31,44 @@ int main(int argc, const char *argv[]) {
   id<MTLCommandBuffer> command_buffer = [queue commandBuffer];
   id<MTLComputeCommandEncoder> encoder = [command_buffer computeCommandEncoder];
 
-  {};
+  float x_data[] = {1, 2, 3, 4};
+  float y_data[] = {5, 6, 7, 8};
+  float z_data[] = {0, 0, 0, 0};
+
+  size_t len = 4;
+  id<MTLBuffer> x = [device newBufferWithBytes:x_data
+                                        length:sizeof(x_data)
+                                       options:MTLResourceStorageModeShared];
+  id<MTLBuffer> y = [device newBufferWithBytes:y_data
+                                        length:sizeof(y_data)
+                                       options:MTLResourceStorageModeShared];
+  id<MTLBuffer> z = [device newBufferWithBytes:z_data
+                                        length:sizeof(z_data)
+                                       options:MTLResourceStorageModeShared];
+
+  {
+    [encoder setBuffer:x offset:0 atIndex:0];
+    [encoder setBuffer:y offset:0 atIndex:1];
+    [encoder setBuffer:z offset:0 atIndex:2];
+    [encoder setComputePipelineState:pipeline];
+    [encoder dispatchThreadgroups:MTLSizeMake(1, 1, 1)
+            threadsPerThreadgroup:MTLSizeMake(len, 1, 1)];
+  };
 
   [encoder endEncoding];
   [command_buffer commit];
   [command_buffer waitUntilCompleted];
+
+  MTLCommandBufferStatus status = (MTLCommandBufferStatus)[command_buffer status];
+  if (status != MTLCommandBufferStatusCompleted) {
+    fprintf(stderr, "%s: command buffer failed with status %lu\n", __func__, status);
+    exit(1);
+  }
+
+  for (size_t i = 0; i < len; i++) {
+    uint8_t *ptr_z = z.contents;
+    printf("%d\n", ptr_z[i]);
+  }
 
   printf("done\n");
   return 0;
