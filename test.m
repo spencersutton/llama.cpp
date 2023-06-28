caf187118e01a93ac0d4d28696fb4bebc92c5953
Spencer Sutton <Spencer.Sutton@precisely.com>
2023-06-28 10:32:37 -0400

    test

--- test.m
+++ test.m
@@ -1,13 +1,17 @@
 #import <Foundation/Foundation.h>
 
 #import <Metal/Metal.h>
-#import <MetalPerformanceShaders/MetalPerformanceShaders.h>
+#include <assert.h>
 
-#define CHECK_ERROR(ptr)                                                                         \
-  if (!(ptr) || error) {                                                                         \
-    fprintf(stderr, "%s:%d: error: %s\n", __FILE__, __LINE__, [[error description] UTF8String]); \
-    exit(1);                                                                                     \
+void add_arrays(const float* inA, const float* inB, float* result, int length) {
+  for (int index = 0; index < length; index++) {
+    result[index] = inA[index] + inB[index];
   }
+}
+
+// The number of floats in each array, and the size of the arrays in bytes.
+const unsigned int arrayLength = 16;
+const unsigned int bufferSize = arrayLength * sizeof(float);
 
 int main(int argc, const char *argv[]) {
   id<MTLDevice> device = MTLCreateSystemDefaultDevice();
@@ -15,60 +19,96 @@ int main(int argc, const char *argv[]) {
   NSError *error = nil;
 
   NSURL *url = [NSURL fileURLWithPath:@"default.metallib"];
-  printf("loading library from %s\n", [[url absoluteString] UTF8String]);
   id<MTLLibrary> library = [device newLibraryWithURL:url error:&error];
-  CHECK_ERROR(library);
+  if (!library || error) {
+    printf("Error occurred when creating default library: %s\n", [[error description] UTF8String]);
+    NSLog(@"Failed to find the default library.");
+    exit(EXIT_FAILURE);
+  }
 
   id<MTLFunction> function = [library newFunctionWithName:@"add_arrays"];
-  CHECK_ERROR(function);
+  if (!function) {
+    NSLog(@"Failed to find the adder function.");
+    exit(EXIT_FAILURE);
+  }
 
+  // Create a compute pipeline state object.
   id<MTLComputePipelineState> pipeline = [device newComputePipelineStateWithFunction:function
                                                                                error:&error];
-  CHECK_ERROR(pipeline)
-
-  id<MTLCommandQueue> queue = [device newCommandQueue];
-
-  id<MTLCommandBuffer> command_buffer = [queue commandBuffer];
-  id<MTLComputeCommandEncoder> encoder = [command_buffer computeCommandEncoder];
-
-  float x_data[] = {1, 2, 3, 4};
-  float y_data[] = {5, 6, 7, 8};
-  float z_data[] = {0, 0, 0, 0};
-
-  size_t len = 4;
-  id<MTLBuffer> x = [device newBufferWithBytes:x_data
-                                        length:sizeof(x_data)
-                                       options:MTLResourceStorageModeShared];
-  id<MTLBuffer> y = [device newBufferWithBytes:y_data
-                                        length:sizeof(y_data)
-                                       options:MTLResourceStorageModeShared];
-  id<MTLBuffer> z = [device newBufferWithBytes:z_data
-                                        length:sizeof(z_data)
-                                       options:MTLResourceStorageModeShared];
-
-  {
-    [encoder setBuffer:x offset:0 atIndex:0];
-    [encoder setBuffer:y offset:0 atIndex:1];
-    [encoder setBuffer:z offset:0 atIndex:2];
-    [encoder setComputePipelineState:pipeline];
-    [encoder dispatchThreads:MTLSizeMake(1, 1, 1) threadsPerThreadgroup:MTLSizeMake(1, 1, 1)];
-  };
-
-  [encoder endEncoding];
-  [command_buffer commit];
-  [command_buffer waitUntilCompleted];
-
-  MTLCommandBufferStatus status = (MTLCommandBufferStatus)[command_buffer status];
-  if (status != MTLCommandBufferStatusCompleted) {
-    fprintf(stderr, "%s: command buffer failed with status %lu\n", __func__, status);
-    exit(1);
+  if (!pipeline) {
+    //  If the Metal API validation is enabled, you can find out more
+    //  information about what went wrong.  (Metal API validation is enabled
+    //  by default when a debug build is run from Xcode)
+    NSLog(@"Failed to created pipeline state object, error %@.", error);
+    exit(EXIT_FAILURE);
   }
 
-  for (size_t i = 0; i < len; i++) {
-    uint8_t *ptr_z = z.contents;
-    printf("%d\n", ptr_z[i]);
+  id<MTLCommandQueue> commandQueue = [device newCommandQueue];
+  assert(commandQueue);
+  if (!commandQueue) {
+    NSLog(@"Failed to find the command queue.");
+    exit(EXIT_FAILURE);
   }
 
-  printf("done\n");
+  // Create buffers to hold data
+  // Allocate three buffers to hold our initial data and the result.
+  id<MTLBuffer> bufferA = [device newBufferWithLength:bufferSize
+                                              options:MTLResourceStorageModeShared];
+  id<MTLBuffer> bufferB = [device newBufferWithLength:bufferSize
+                                              options:MTLResourceStorageModeShared];
+  id<MTLBuffer> bufferResult = [device newBufferWithLength:bufferSize
+                                                   options:MTLResourceStorageModeShared];
+
+  for (unsigned long index = 0; index < arrayLength; index++) {
+    float* dataPtr = bufferA.contents;
+    dataPtr[index] = (float)rand() / (float)(RAND_MAX);
+    dataPtr = bufferB.contents;
+    dataPtr[index] = (float)rand() / (float)(RAND_MAX);
+  }
+
+  // Send a command to the GPU to perform the calculation.
+  // Create a command buffer to hold commands.
+  id<MTLCommandBuffer> commandBuffer = [commandQueue commandBuffer];
+  assert(commandBuffer);
+
+  // Start a compute pass.
+  id<MTLComputeCommandEncoder> computeEncoder = [commandBuffer computeCommandEncoder];
+  assert(computeEncoder);
+
+  // Encode the pipeline state object and its parameters.
+  [computeEncoder setComputePipelineState:pipeline];
+  [computeEncoder setBuffer:bufferA offset:0 atIndex:0];
+  [computeEncoder setBuffer:bufferB offset:0 atIndex:1];
+  [computeEncoder setBuffer:bufferResult offset:0 atIndex:2];
+
+  // Encode the compute command.
+  [computeEncoder dispatchThreads:MTLSizeMake(arrayLength, 1, 1)
+            threadsPerThreadgroup:MTLSizeMake(1, 1, 1)];
+
+  // End the compute pass.
+  [computeEncoder endEncoding];
+
+  // Execute the command.
+  [commandBuffer commit];
+
+  // Normally, you want to do other work in your app while the GPU is running,
+  // but in this example, the code simply blocks until the calculation is
+  // complete.
+  [commandBuffer waitUntilCompleted];
+
+  float* a = bufferA.contents;
+  float* b = bufferB.contents;
+  float* result = bufferResult.contents;
+
+  for (unsigned long index = 0; index < arrayLength; index++) {
+    if (result[index] != (a[index] + b[index])) {
+      printf("Compute ERROR: index=%lu result=%g vs %g=a+b\n", index, result[index],
+             a[index] + b[index]);
+      assert(result[index] == (a[index] + b[index]));
+    }
+  }
+  printf("Compute results as expected\n");
+
+  NSLog(@"Execution finished");
   return 0;
 }
